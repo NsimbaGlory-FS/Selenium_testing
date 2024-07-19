00c54e45967048fa80bb40f5125eb42e9e9ec02e
Glory Dounce <glorynsimba@Glorys-MBP.lan>
2024-07-19 10:08:13 -0400

    push

--- external.test.js
+++ external.test.js
@@ -1,14 +1,18 @@
 require("chromedriver");
 const { Builder, By, Key, until } = require("selenium-webdriver");
 
-const driver = new Builder().forBrowser("chrome").build();
+async function chrome_test() {
+  const driver = new Builder().forBrowser("chrome").build();
 
-driver
-  .get("http://www.google.com/ncr")
-  .then((_) =>
-    driver.findElement(By.name("q")).sendKeys("nicholas cage", Key.RETURN)
-  )
-  .then((_) =>
-    driver.wait(until.titleIs("nicholas cage - Google Search"), 1000)
-  )
-  .then((_) => driver.quit());
+  await driver.get("http://www.google.com/ncr");
+
+  await driver.findElement(By.name("q")).sendKeys("nicholas cage", Key.RETURN);
+
+  await driver.wait(until.titleIs("nicholas cage - Google Search"), 1000);
+
+  setInterval(function () {
+    driver.quit();
+  }, 1000);
+}
+
+chrome_test();
